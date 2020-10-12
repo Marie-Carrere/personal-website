f4aa66da50f961ed5db68337a2a230884048210e
Marie-Carrere <marie.carrere@icloud.com>
2020-10-12 11:31:56 +0200

    :art: Update Hero component

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -21,7 +21,7 @@ const Contact = () => {
               target="_blank"
               rel="noopener noreferrer"
               className="cta-btn cta-btn--resume"
-              href={email ? `mailto:${email}` : 'marie.carrere@zenika.com'}
+              href={email ? `mailto:${email}` : ''}
             >
               {btn || "Let's Talk"}
             </a>
